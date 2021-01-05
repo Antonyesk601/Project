3659442a95890e1fb7cf7763a59217777b017bbe
Antonyesk601 <40663824+Antonyesk601@users.noreply.github.com>
2021-01-05 20:19:43 +0200

    tarek level possibly

--- Scripts/tareklevel.py
+++ Scripts/tareklevel.py
@@ -39,22 +39,22 @@ screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("A wizard`s tale")
 
 # load images
-bkg_img = pygame.image.load("../level 4 images/level 4 intro.jpg")
+bkg_img = pygame.image.load("level 4 images/level 4 intro.jpg")
 bkg = pygame.transform.scale(bkg_img, (screen_width,screen_height))
-bg_img = pygame.image.load("../level 4 images/NightForest/Forestgrey.png") # Background
+bg_img = pygame.image.load("level 4 images/NightForest/Forestgrey.png") # Background
 bg = pygame.transform.scale(bg_img,(screen_width,screen_height)) #fix background scale
-restart_img = pygame.image.load("../level 4 images/restart_btn.png")
-start_img = pygame.image.load("../level 4 images/start_btn.png")
-exitbtn_img = pygame.image.load("../level 4 images/exit_btn.png")
+restart_img = pygame.image.load("level 4 images/restart_btn.png")
+start_img = pygame.image.load("level 4 images/start_btn.png")
+exitbtn_img = pygame.image.load("level 4 images/exit_btn.png")
 
 #load sounds
-pygame.mixer.music.load("../level 4 sounds/level 4 original.mp3")
+pygame.mixer.music.load("level 4 sounds/level 4 original.mp3")
 pygame.mixer.music.play(-1, 0.0, 3000)
 pygame.mixer.music.set_volume(0.2)
-coin_fx = pygame.mixer.Sound("../level 4 sounds/coin.mp3")
-jump_fx = pygame.mixer.Sound("../level 4 sounds/jump.mp3")
+coin_fx = pygame.mixer.Sound("level 4 sounds/coin.mp3")
+jump_fx = pygame.mixer.Sound("level 4 sounds/jump.mp3")
 jump_fx.set_volume(0.5) #halves the original volume
-game_over_fx = pygame.mixer.Sound("../level 4 sounds/game_over.mp3")
+game_over_fx = pygame.mixer.Sound("level 4 sounds/game_over.mp3")
 
 
 def draw_text(text, font, text_col, x, y):
@@ -62,7 +62,7 @@ def draw_text(text, font, text_col, x, y):
     screen.blit(img, (x ,y))
 
 #reset level funciton
-def reset_level(level):
+def reset_level(level,world_data):
     #reset player position and destroy all entities
     player.reset(screen_width - 50, screen_height - 170)
     slime_group.empty()
@@ -95,8 +95,8 @@ coin_group.add(score_coin)
 
 
 #load in level data and create world
-if path.exists(f"../level{level}_data"):
-    pickle_in = open(f"../level{level}_data", "rb") #open file and read binary to process whatever info is in this file
+if path.exists(f"level{level}_data"):
+    pickle_in = open(f"level{level}_data", "rb") #open file and read binary to process whatever info is in this file
     world_data = pickle.load(pickle_in)
 world =definitions.World(world_data) #(runs once outside game loop)
 
@@ -107,7 +107,7 @@ player = definitions.Player(screen_width - 50, screen_height - 170)
 restart_button = definitions.TuTButton(screen_width // 2 - 50, screen_height // 2 + 100, restart_img)
 start_button = definitions.TuTButton(screen_width // 2 - 350, screen_height // 2, start_img)
 exit_button = definitions.TuTButton(screen_width // 2 + 150, screen_height // 2, exitbtn_img)
-def run(main_menu,game_over,world,score,level):
+def run(main_menu,game_over,world,score,level,world_data):
     # game loop
     run = True
     while run:
@@ -151,31 +151,10 @@ def run(main_menu,game_over,world,score,level):
                 coin_collision = []
                 if restart_button.draw(): #used in if as it draws and returns true/false
                     #recreate the level to reload collected coins before death
-                    world_data = []
-                    world = reset_level(level)
+                    world = reset_level(level,world_data)
                     game_over = 0
                     score = 0
                     pygame.mixer.music.play(-1, 0.0, 3000)
-
-
-            #if player has completed the level
-            if game_over == 1:
-                #reset game and go to next level
-                level += 1
-                if level <= max_levels:
-                    #reset level to get new level data
-                    world_data = []
-                    world = reset_level(level) # loads the next level data
-                    game_over = 0 #reset game over variable
-                else:
-                    draw_text("YOU WIN!", font, blue, (screen_width // 2) - 140, screen_height // 2)
-                    if restart_button.draw():
-                        level = 1
-                        #reset level
-                        world_data = []
-                        world = reset_level(level)
-                        game_over = 0
-                        score = 0
 
 
         #close event handler
@@ -186,4 +165,3 @@ def run(main_menu,game_over,world,score,level):
         pygame.display.update()
 
     pygame.quit()
-run(main_menu,game_over,world,score,level)
